358adf0fd54d434041af570440fa14bc2e3a3919
pokenem <andreystepanov0412@gmail.com>
2023-09-18 09:44:06 +0300

    some logic

--- lib/main.dart
+++ lib/main.dart
@@ -28,18 +28,40 @@ class _MyAppState extends State<MyApp> {
   double _currentSliderValue1 = 0;
   double _currentSliderValue2 = 0;
   double _currentSliderValue3 = 0;
+  double _currentSliderValue4 = 0;
   String dropdownValue = list.first;
+  late TextEditingController _controller1;
+  late TextEditingController _controller2;
+  late TextEditingController _controller3;
+  late TextEditingController _controller4;
 
+  @override
+  void initState() {
+    super.initState();
+    _controller1 = TextEditingController();
+    _controller2 = TextEditingController();
+    _controller3 = TextEditingController();
+    _controller4 = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _controller1.dispose();
+    _controller2.dispose();
+    _controller3.dispose();
+    _controller4.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Цветовые модели'),
+        title: const Text('Цветовые модели'),
       ),
       body: Column(
         children: [
           Padding(
-            padding: EdgeInsets.only(top: 100),
+            padding: const EdgeInsets.only(top: 100),
             child: Align(
               alignment: Alignment.center,
               child: SizedBox(
@@ -52,7 +74,7 @@ class _MyAppState extends State<MyApp> {
             ),
           ),
           Padding(
-            padding: EdgeInsets.only(top: 20),
+            padding: const EdgeInsets.only(top: 20),
             child: DropdownButton(
               value: dropdownValue,
               items: list.map<DropdownMenuItem<String>>((String value) {
@@ -64,12 +86,14 @@ class _MyAppState extends State<MyApp> {
               onChanged: (String? value) {
                 setState(() {
                   dropdownValue = value!;
+                  changeColor();
+
                 });
               },
             ),
           ),
             Padding(
-              padding: EdgeInsets.only(
+              padding: const EdgeInsets.only(
                 left: 20,
                 right: 20,
                 top: 50,
@@ -83,12 +107,13 @@ class _MyAppState extends State<MyApp> {
                 onChanged: (double value) {
                   setState(() {
                     _currentSliderValue1 = value;
+                    changeSliderColor();
                   });
                 },
               ),
             ),
           Padding(
-            padding: EdgeInsets.only(
+            padding: const EdgeInsets.only(
               left: 20,
               right: 20,
               top: 50,
@@ -102,12 +127,13 @@ class _MyAppState extends State<MyApp> {
               onChanged: (double value) {
                 setState(() {
                   _currentSliderValue2 = value;
+                  changeSliderColor();
                 });
               },
             ),
           ),
           Padding(
-            padding: EdgeInsets.only(
+            padding: const EdgeInsets.only(
               left: 20,
               right: 20,
               top: 50,
@@ -121,12 +147,96 @@ class _MyAppState extends State<MyApp> {
               onChanged: (double value) {
                 setState(() {
                   _currentSliderValue3 = value;
+                  changeSliderColor();
                 });
               },
             ),
           ),
+          if(dropdownValue == 'CMYK')
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 20,
+                right: 20,
+                top: 50,
+              ),
+              child: Slider(
+                value: _currentSliderValue4,
+                min: map[dropdownValue].leftLimit,
+                max: map[dropdownValue].rightLimit,
+                divisions: map[dropdownValue].cntDiv,
+                label: _currentSliderValue4.round().toString(),
+                onChanged: (double value) {
+                  setState(() {
+                    _currentSliderValue4 = value;
+                    changeSliderColor();
+                  });
+                },
+              ),
+            ),
         ],
       ),
     );
+  }
+  void changeSliderColor() {
+    switch(dropdownValue){
+      case 'RGB' : {
+        RgbColor rgbColor = RgbColor(_currentSliderValue1, _currentSliderValue2, _currentSliderValue3);
+        crntColor = rgbColor.toColor();
+      }
+      break;
+      case 'CMYK' : {
+        CmykColor cmykColor = CmykColor(_currentSliderValue1, _currentSliderValue2, _currentSliderValue3, _currentSliderValue4);
+        crntColor = cmykColor.toColor();
+      }
+      break;
+      case 'HSV' : {
+        crntColor = HSVColor.fromAHSV(1,_currentSliderValue1, _currentSliderValue2, _currentSliderValue3).toColor();
+      }
+      break;
+      case 'HLS' : {
+        crntColor = HslColor(_currentSliderValue1, _currentSliderValue2, _currentSliderValue3).toColor();
+      }
+      break;
+      case 'XYZ' : {
+        crntColor = XyzColor(_currentSliderValue1, _currentSliderValue2, _currentSliderValue3).toColor();
+      }
+      break;
+      case 'LAB' : {
+        crntColor = LabColor(_currentSliderValue1, _currentSliderValue2, _currentSliderValue3).toColor();
+      }
+      break;
+    }
+  }
+  void changeColor(){
+    switch(dropdownValue){
+      case 'RGB' : {
+        RgbColor.fromColor(crntColor);
+      }
+      break;
+      case 'CMYK' : {
+        CmykColor cmykColor = CmykColor.fromColor(crntColor);
+        _currentSliderValue1 = cmykColor.cyan.toDouble();
+        _currentSliderValue2 = cmykColor.magenta.toDouble();
+        _currentSliderValue3 = cmykColor.yellow.toDouble();
+        _currentSliderValue4 = cmykColor.black.toDouble();
+      }
+      break;
+      case 'HSV' : {
+        crntColor = HSVColor.fromAHSV(1,_currentSliderValue1, _currentSliderValue2, _currentSliderValue3).toColor();
+      }
+      break;
+      case 'HLS' : {
+        crntColor = HslColor(_currentSliderValue1, _currentSliderValue2, _currentSliderValue3).toColor();
+      }
+      break;
+      case 'XYZ' : {
+        crntColor = XyzColor(_currentSliderValue1, _currentSliderValue2, _currentSliderValue3).toColor();
+      }
+      break;
+      case 'LAB' : {
+        crntColor = LabColor(_currentSliderValue1, _currentSliderValue2, _currentSliderValue3).toColor();
+      }
+      break;
+    }
   }
 }
--- lib/models/color_classes.dart
+++ lib/models/color_classes.dart
@@ -11,8 +11,8 @@ class RgbClor{
 class CmykClor{
   int get cntColors => 4;
   double get leftLimit => 0;
-  int get cntDiv => 256;
-  double get rightLimit => 255;
+  int get cntDiv => 100;
+  double get rightLimit => 100;
   final double? c;
   final double? m;
   final double? y;
@@ -28,4 +28,34 @@ class HsvClor{
   final double? s;
   final double? v;
   const HsvClor(this.h, this.s, this.v);
+}
+class HlsClor{
+  int get cntDiv => 100;
+  int get cntColors => 3;
+  double get leftLimit => 0;
+  double get rightLimit => 1;
+  final double? h;
+  final double? l;
+  final double? s;
+  const HlsClor(this.h, this.l, this.s);
+}
+class XyzClor{
+  int get cntDiv => 100;
+  int get cntColors => 3;
+  double get leftLimit => 0;
+  double get rightLimit => 1;
+  final double? x;
+  final double? y;
+  final double? z;
+  const XyzClor(this.x, this.y, this.z);
+}
+class LabClor{
+  int get cntDiv => 100;
+  int get cntColors => 3;
+  double get leftLimit => 0;
+  double get rightLimit => 1;
+  final double? l;
+  final double? a;
+  final double? b;
+  const LabClor(this.l, this.a, this.b);
 }
